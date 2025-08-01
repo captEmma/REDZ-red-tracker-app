bf690c6c2280051e37e1db56e19e43a7d2cb2185
Dorina <majorosdorina02@gmail.com>
2025-08-01 13:52:12 +0000

    example

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Performance from './components/Performance'
 
 function App() {
+  let list = ["stock1", "stock2"];
+
   return (
     <>
       {/* TODO: create homepage */}
+      <Performance title="example1" list={list}></Performance>
     </>
   )
 }
