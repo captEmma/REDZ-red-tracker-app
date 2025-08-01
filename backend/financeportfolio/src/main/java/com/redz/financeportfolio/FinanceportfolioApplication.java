71f6403b4d18d14ba63a517af0b0838f56182313
captEmma <benko99emma@gmail.com>
2025-08-01 09:13:23 +0000

    init backend project

---
+++ backend/financeportfolio/src/main/java/com/redz/financeportfolio/FinanceportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.redz.financeportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceportfolioApplication.class, args);
+	}
+
+}
