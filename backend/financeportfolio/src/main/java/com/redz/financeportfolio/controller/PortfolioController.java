bdaf9914d49f560b5c96464de0a71e5f845abb77
captEmma <benko99emma@gmail.com>
2025-08-04 15:11:48 +0000

    #9 #10 add controller to YahooFinanceService

--- backend/financeportfolio/src/main/java/com/redz/financeportfolio/controller/PortfolioController.java
+++ backend/financeportfolio/src/main/java/com/redz/financeportfolio/controller/PortfolioController.java
@@ -1,5 +1,20 @@
 package com.redz.financeportfolio.controller;
 
+import com.redz.financeportfolio.service.YahooFinanceService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@RestController
+@RequestMapping("/stocks")
 public class PortfolioController {
+    private YahooFinanceService yahooFinanceService;
+
+    @GetMapping("/{stock}")
+    public Map<String, Object> getStockData(
+            @PathVariable String stock,
+            @RequestParam String period,
+            @RequestParam String interval){
+        return yahooFinanceService.getStockData(stock, period, interval);
+    }
 }
