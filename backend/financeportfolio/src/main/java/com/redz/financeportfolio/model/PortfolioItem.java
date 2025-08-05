6f034ba0804afe18d50ab0507c5e50ad7d611658
captEmma <benko99emma@gmail.com>
2025-08-05 16:48:40 +0000

    #7 set up PortfolioItem model

--- backend/financeportfolio/src/main/java/com/redz/financeportfolio/model/PortfolioItem.java
+++ backend/financeportfolio/src/main/java/com/redz/financeportfolio/model/PortfolioItem.java
@@ -1,4 +1,41 @@
 package com.redz.financeportfolio.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class PortfolioItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String symbol;
+    //TODO: buy() -> increase shares
+    private double shares;
+    private double purchasePrice;
+    //TODO: figure out conversion from SQL DATETIME
+    private String purchaseDate;
+
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public double getShares() {
+        return shares;
+    }
+
+    public void setShares(double shares) {
+        this.shares = shares;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
