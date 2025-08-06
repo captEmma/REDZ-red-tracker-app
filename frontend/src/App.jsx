6cc2fb68354aef7dad29e6fb6207843eaa34d802
Rachael Okuwa <you@example.com>
2025-08-06 15:50:26 +0000

    Initial ui development for skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,13 @@
-import './App.css'
-import Performance from './components/Performance'
-
+import "./App.css";
+import Performance from "./components/Performance";
+import Skeleton from "./components/Skeleton";
 function App() {
   return (
     <>
       {/* TODO: create homepage */}
-      <div id = "top">
-          <p style={{textAlign: "left"}}>Available Balance </p>
-          <p style={{textAlign: "left", display: "inline-block"}}><b>$40000</b></p>
-          <button class="button" onclick="location.href='default.html';" style={{float: "right"}}>Manage Funds</button><br></br>
-          {/* <p style="display:inline-block;*/}
-      </div>
-      <div id = "mainContainer">  
-        <div id = "container">
-          <div id="left-container">
-            left container
-            <img src = "../line-graph.png"></img>
-          </div>
-        </div>
-        
-      </div>
-    
-      
-
+      <Skeleton />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
