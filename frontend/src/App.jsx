1e7c5bee6bc7ef908eab292184b8529a22050504
Rachael Okuwa <you@example.com>
2025-08-06 09:59:04 +0000

    Initial implementation of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,27 @@ import './App.css'
 import Performance from './components/Performance'
 
 function App() {
-  let list = ["stock1", "stock2"];
-
   return (
     <>
       {/* TODO: create homepage */}
-      <Performance title="example1" list={list}></Performance>
+      <div id = "top">
+          <p style={{textAlign: "left"}}>Available Balance </p>
+          <p style={{textAlign: "left", display: "inline-block"}}><b>$40000</b></p>
+          <button class="button" onclick="location.href='default.html';" style={{float: "right"}}>Manage Funds</button><br></br>
+          {/* <p style="display:inline-block;*/}
+      </div>
+      <div id = "mainContainer">  
+        <div id = "container">
+          <div id="left-container">
+            left container
+            <img src = "../line-graph.png"></img>
+          </div>
+        </div>
+        
+      </div>
+    
+      
+
     </>
   )
 }
