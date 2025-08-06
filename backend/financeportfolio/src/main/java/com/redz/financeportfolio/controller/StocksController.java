395540b4705cda2a405d4509a042e9a48d1e8168
Zsombor <you@example.com>
2025-08-06 11:07:55 +0000

    #42 Finalize

--- backend/financeportfolio/src/main/java/com/redz/financeportfolio/controller/StocksController.java
+++ backend/financeportfolio/src/main/java/com/redz/financeportfolio/controller/StocksController.java
@@ -1,26 +1,27 @@
 package com.redz.financeportfolio.controller;
 
+import com.redz.financeportfolio.model.StockData;
 import com.redz.financeportfolio.service.YahooFinanceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/stocks")
-public class PortfolioController {
+public class StocksController {
     @Autowired
     private YahooFinanceService yahooFinanceService;
 
-    public PortfolioController(YahooFinanceService yahooFinanceService){
+    public StocksController(YahooFinanceService yahooFinanceService){
         this.yahooFinanceService = yahooFinanceService;
     }
 
     @GetMapping("/{stock}")
-    public Map<String, Object> getStockData(
+    public ResponseEntity<StockData> getStockData(
             @PathVariable String stock,
             @RequestParam(defaultValue = "1d") String period,
-            @RequestParam(defaultValue = "1d") String interval){
-        return yahooFinanceService.getStockData(stock, period, interval);
+            @RequestParam(defaultValue = "1d") String interval) {
+        StockData stockData = yahooFinanceService.getStockData(stock, period, interval);
+        return ResponseEntity.ok(stockData);
     }
 }
