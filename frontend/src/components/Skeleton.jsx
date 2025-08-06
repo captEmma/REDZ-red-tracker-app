338a321f2253bc4388da00d7a5e408898df54918
Dorina <majorosdorina02@gmail.com>
2025-08-06 16:27:40 +0000

    fix header

--- frontend/src/components/Skeleton.jsx
+++ frontend/src/components/Skeleton.jsx
@@ -5,22 +5,15 @@ import Col from "react-bootstrap/esm/Col";
 const Skeleton = () => {
   return (
     <>
-      <Container fluid style={{ width: "100%" }}>
-        <Row id="top">
+      <Container fluid style={{ width: "100%", padding: "0px" }}>
+        <Row className=" mx-0" id="top">
           <Col>
-            {/* <p style={{ textAlign: "left", display: "inline-block" }}>
-              Available Balance <b>$40000</b>
-            </p> */}
             <div>
               Available Balance <b>$40000</b>
             </div>
           </Col>
           <Col>
-            <button
-              class="button"
-              onclick="location.href='default.html';"
-              style={{ float: "right" }}
-            >
+            <button class="button" onclick="location.href='default.html';" style={{ float: "right" }}>
               Manage Funds
             </button>
           </Col>
@@ -40,34 +33,36 @@ const Skeleton = () => {
         </button>
         <br></br>
       </div> */}
-      <div id="main-container">
-        <div id="left-container">
-          <h2>
-            <b>Gainers</b> {/*should be white usnure why its black */}
-          </h2>
-          <br />
+      <div className="center-content">
+        <div id="main-container">
+          <div id="left-container">
+            <h2>
+              <b>Gainers</b> {/*should be white usnure why its black */}
+            </h2>
+            <br />
 
-          <img
-            src="https://cdn.sanity.io/images/599r6htc/regionalized/128e6a1b9aba13b48886e276b4c02e5c6c68ec01-1108x1108.png?w=1200&q=70&fit=max&auto=format"
-            height="200px"
-            width="200px"
-          ></img>
-          <br />
-          <br />
+            <img
+              src="https://cdn.sanity.io/images/599r6htc/regionalized/128e6a1b9aba13b48886e276b4c02e5c6c68ec01-1108x1108.png?w=1200&q=70&fit=max&auto=format"
+              height="200px"
+              width="200px"
+            ></img>
+            <br />
+            <br />
 
-          <h2>
-            <b>Losers</b> {/*should be white usnure why its black */}
-          </h2>
+            <h2>
+              <b>Losers</b> {/*should be white usnure why its black */}
+            </h2>
 
-          <br />
-          <img
-            src="https://cdn.sanity.io/images/599r6htc/regionalized/128e6a1b9aba13b48886e276b4c02e5c6c68ec01-1108x1108.png?w=1200&q=70&fit=max&auto=format"
-            height="200px"
-            width="200px"
-          ></img>
+            <br />
+            <img
+              src="https://cdn.sanity.io/images/599r6htc/regionalized/128e6a1b9aba13b48886e276b4c02e5c6c68ec01-1108x1108.png?w=1200&q=70&fit=max&auto=format"
+              height="200px"
+              width="200px"
+            ></img>
+          </div>
+          <div id="right-container"></div>
+          <div id="b-s-container"></div>
         </div>
-        <div id="right-container"></div>
-        <div id="b-s-container"></div>
       </div>
     </>
   );
