6cf6c0f36f6453736e99bcb7a85a8223ed063c06
Dorina <majorosdorina02@gmail.com>
2025-08-07 13:08:53 +0000

    create modal and form for buying/selling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,62 @@
+import { useState } from "react";
 import "./App.css";
-import { useService } from "./api/Service";
-import { useEffect, useState } from "react";
 import Investment from "./components/Investment";
+import ManageStocks from "./components/ManageStocks";
+import Modal from "./components/Modal";
+import Button from "./components/Button";
 
 function App() {
-  const { getStocks, getStocksLastXDays } = useService();
-  const [stocks, setStocks] = useState();
-  const [multipleStocks, setMultipleStocks] = useState();
+  const [isBuyingOpen, setIsBuyingOpen] = useState(false);
+  const [isSellingOpen, setIsSellingOpen] = useState(false);
 
-  useEffect(() => {
-    async function getStocksData() {
-      try {
-        const stocks = await getStocks();
-        setStocks(stocks);
-      } catch (error) {
-        console.log(error.response.data.errors);
-      }
-    }
-    if (!stocks) {
-      getStocksData();
-    }
-  }, [getStocks, stocks]);
-
-  useEffect(() => {
-    async function getMultipleStocks() {
-      try {
-        const multipleStocks = await getStocksLastXDays(7);
-        setMultipleStocks(multipleStocks);
-      } catch (error) {
-        console.log(error.response.data.errors);
-      }
-    }
-    if (!multipleStocks) {
-      getMultipleStocks();
-    }
-  }, [getStocksLastXDays, multipleStocks]);
-
-  console.log(stocks);
-  console.log(multipleStocks);
+  const options = ["Company1", "Company2", "Company3"];
 
   return (
     <>
       <Investment />
+      <ManageStocks setIsBuyingOpen={setIsBuyingOpen} setIsSellingOpen={setIsSellingOpen} />
+      {isBuyingOpen && (
+        <Modal title={"Buy shares"} onClose={() => setIsBuyingOpen(false)}>
+          <form>
+            <div className="flex-downward">
+              <label htmlFor="symbol">Company</label>
+              <select name="symbol" id="symbol">
+                {options.map((option) => (
+                  <option value={option}>{option}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="flex-downward">
+              <label htmlFor="amount">Amount</label>
+              <div className="currency">
+                <input name="amount" id="amount" type="text" />
+              </div>
+            </div>
+            <Button title="Buy" />
+          </form>
+        </Modal>
+      )}
+      {isSellingOpen && (
+        <Modal title={"Sell shares"} onClose={() => setIsSellingOpen(false)}>
+          <form>
+            <div className="flex-downward">
+              <label htmlFor="symbol">Company</label>
+              <select name="symbol" id="symbol">
+                {options.map((option) => (
+                  <option value={option}>{option}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="flex-downward">
+              <label htmlFor="shares">Amount of Shares</label>
+              <input name="shares" id="shares" type="text" />
+            </div>
+            <Button title="Sell" />
+          </form>
+        </Modal>
+      )}
     </>
   );
 }
--- frontend/src/components/ManageStocks.jsx
+++ frontend/src/components/ManageStocks.jsx
@@ -1,18 +1,18 @@
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import StockButton from "./StockButton";
 
-const ManageStocks = () => {
-    return (
-        <div className="manage-stocks-bg">
-            <div>
-                <StockButton icon={faPlus} title="Buy stocks" />
-            </div>
-            <div className="or center-content">or</div>
-            <div>
-                <StockButton icon={faMinus} title="Sell stocks" />
-            </div>
-        </div>
-    );
+const ManageStocks = ({ setIsBuyingOpen, setIsSellingOpen }) => {
+  return (
+    <div className="manage-stocks-bg">
+      <div>
+        <StockButton icon={faPlus} title="Buy shares" onClick={() => setIsBuyingOpen(true)} />
+      </div>
+      <div className="or center-content">or</div>
+      <div>
+        <StockButton icon={faMinus} title="Sell shares" onClick={() => setIsSellingOpen(true)} />
+      </div>
+    </div>
+  );
 };
 
 export default ManageStocks;
