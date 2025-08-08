0c513088e1aa693b47d122e6e57a9365e789dd01
Rachael Okuwa <you@example.com>
2025-08-08 13:11:42 +0000

    changes to header- adding endpoint

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,15 +2,37 @@ import Container from "react-bootstrap/esm/Container";
 import "./Skeleton.css";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import { useService } from "../api/Service";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const { getUser } = useService();
+  const [user, setUsers] = useState();
+
+  useEffect(() => {
+    async function getUserData() {
+      try {
+        const user = await getUser();
+        setUsers(user);
+      } catch (error) {
+        console.log(error.response.data.errors);
+      }
+    }
+    if (!user) {
+      getUserData();
+    }
+  }, [getUser, user]);
+
+  console.log(user);
   return (
     <Container fluid style={{ width: "100%", padding: "0px" }}>
       <Row className="mx-0" id="top">
         <Col>
-          <div>
-            Available Balance: <b>$40000</b>
-          </div>
+          {user && (
+            <div>
+              Available Balance: <b>${user.cash}</b>
+            </div>
+          )}
         </Col>
         <Col>
           <button className="funds-button">Manage Funds</button>
